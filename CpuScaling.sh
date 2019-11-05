1ed2aa7ba8adc52020ac2dbf7f54590302f3b4f7
webmedi <lovemika9528202605@yahoo.co.jp>
2019-11-06 05:35:05 +0900

    Change CPU パフォーマンスに差違があった場合に, 最後に変更した結果を shellScript と同ディレクトリに書き出すように変更。

--- CpuScaling.sh
+++ CpuScaling.sh
@@ -2,7 +2,7 @@
 # /**************************************************************************
 #  *
 #  * init modify  : 2019年10月25日(金)
-#  * modify       : 2019年10月25日(金)
+#  * modify       : 2019年10月27日(日)
 #  * summary      : Raspi3B+ CentOS7 環境の CPU を最大パフォーマンス状態にする。
 #  *              : アーキテクチャ: armv7l
 #  *              : Model name:ARMv7 Processor rev 4 (v7l)
@@ -17,7 +17,11 @@ scriptDir=$(dirname $0)
 selfScriptName=CpuScaling.sh
 selfScriptDir=$scriptDir
 selfScriptExe=$selfScriptDir/$selfScriptName
-scaleCpu=
+scaleCpul=
+isDiffer=false
+cronStdOutLogPath=/root/script/CpuScaling.log
+lastChangeWriteLogName=LastChangeWrite.log
+lastChangeWriteLogPath=$selfScriptDir/$lastChangeWriteLogName
 
 for((i = 0; i < $cpuSize; i++)); do
     scaleCpu=/sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
@@ -30,6 +34,8 @@ for((i = 0; i < $cpuSize; i++)); do
         echo "CPU"$i" : "$(cat $scaleCpu)
         echo "CPU スケーリング実行終了しました。"
 
+        isDiffer=true
+
         #$selfScriptExe
 
     fi
@@ -39,7 +45,7 @@ for((i = 0; i < $cpuSize; i++)); do
 
     fi
 
-    echo $(cat $scaleCpu)
+    echo "CPU"$i" : "$(cat $scaleCpu)
 
     if [ "$(( $cpuSize - 1 ))" = "$i" ]; then
         echo "<----result----/>"
@@ -49,3 +55,8 @@ for((i = 0; i < $cpuSize; i++)); do
 done
 
 echo $cpuSize
+
+if [ "$isDiffer" = "true" ]; then
+    cp -p $cronStdOutLogPath $lastChangeWriteLogPath
+
+fi
